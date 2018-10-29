b6176de9d061f62c88b505bf35d17e2f45133138
Carlos Timaury <timauryc@gmail.com>
2018-10-29 19:52:57 -0300

    added chai and mocha packages

--- test/server.specs.js
+++ test/server.specs.js
@@ -7,3 +7,14 @@ const server = require('../server')
 
 chai.use(chaiHttp);
 
+describe('Server', function () {
+    describe('get a new game (GET /game/:difficulty)', function () {
+        it('should return a new game with all the info needed')
+        it('should allow only allowed difficulties')
+    })
+
+    describe('click on the board (Post /game)', function () {
+        it('should return the actual game with all the info needed')
+        it('should allow only allowed coordinates')
+    })
+})
