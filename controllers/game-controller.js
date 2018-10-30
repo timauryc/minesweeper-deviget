64cd1175b64e9d11251de16bc1dab018c82fb1c0
Carlos Timaury <timauryc@gmail.com>
2018-10-29 21:32:48 -0300

    written ./routes/game.js

---
+++ controllers/game-controller.js
@@ -0,0 +1,7 @@
+const minesweeper = require('minesweeper');
+
+const BoardStateEnum = minesweeper.BoardStateEnum;
+const CellStateEnum = minesweeper.CellStateEnum;
+const CellFlagEnum = minesweeper.CellFlagEnum;
+const Board = minesweeper.Board;
+const Cell = minesweeper.Cell;
---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+	gameController: require('./game-controller')
+};
