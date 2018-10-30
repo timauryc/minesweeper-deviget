a412519af3328595c3cdfac9887a52042f1ca34b
Carlos Timaury <timauryc@gmail.com>
2018-10-29 23:43:32 -0300

    writting the client

--- public/gameClient.js
+++ public/gameClient.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
     console.log("hey, im ready :)")
+
+    let minesLeft
+
     $(".start").click(function () {
         startGame()
     })
+
+    function startGame(){
+        let level = $('#game-level :selected').val()
+        console.log(level)
+    }
 
 });
 
